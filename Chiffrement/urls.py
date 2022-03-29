c00d949754ed089fe523925b69b11f2724618b2f
Hamadé <95353352+K-Hamande@users.noreply.github.com>
2022-03-29 11:28:01 +0000

    Update urls.py

--- Chiffrement/urls.py
+++ Chiffrement/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import Dechiffrer , Chiffrer
 
-
+#chemin correspondant au differnt liens de l'application
 urlpatterns = [
+    
+    #chemin pour le chiffrement 
+    
     path('Chiffrer/',Chiffrer,name="Chiffrer"),
+    
+    #Chemin pour le dechiffrement
+    
     path('Dechiffrer', Dechiffrer,name="Dechiffrer"),
 ]
